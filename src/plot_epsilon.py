a4ce88c4c981cc042dbf30da909cb47396f16ce4
Talon Chandler <talonchandler@talonchandler.com>
2015-07-03 15:16:08 -0700

    Add dynamic plotting functionality with pandas

--- src/plot_epsilon.py
+++ src/plot_epsilon.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 df = pd.DataFrame.from_csv('test/output.csv', parse_dates=False)
 
-plt.figure()
-plt.clf()
-plt.errorbar(df.N,df.SAA_eps_avg,xerr=0,yerr=df.SAA_eps_conf,marker='x',c='r',ls='-')
-#plt.errorbar(df.N,df.SAA_eps_avg_interp,xerr=0,yerr=df.SAA_eps_conf_interp,marker='x',c='g',ls='-')
+select_list = ['demand_type','param1','param2']
+for key, grp in df.groupby(select_list):
+    plt.errorbar(grp['N'], grp['SAA_eps_avg'], yerr=grp['SAA_eps_conf'], label=key)
 
-plt.axis([4, 16, 0.05, 0.25])
+plt.title(select_list)
 plt.xlabel('N')
 #plt.xscale('log')
 plt.ylabel('Epsilon')
-plt.legend( ('SAA_eps_avg','SAA_eps_avg_interp'),\
-             loc='upper right')
+plt.legend(loc='best')    
 
 #plt.show()
 plt.savefig('test/epsilon.png',format='png')
