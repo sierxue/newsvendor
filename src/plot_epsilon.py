5db99763f3c0c7c10123c964dd3e39e6e67a2a55
Talon Chandler <talonchandler@talonchandler.com>
2015-07-03 11:46:35 -0700

    Plot just epsilon, not interpolation

--- src/plot_epsilon.py
+++ src/plot_epsilon.py
@@ -6,7 +6,7 @@ df = pd.DataFrame.from_csv('test/output.csv', parse_dates=False)
 plt.figure()
 plt.clf()
 plt.errorbar(df.N,df.SAA_eps_avg,xerr=0,yerr=df.SAA_eps_conf,marker='x',c='r',ls='-')
-plt.errorbar(df.N,df.SAA_eps_avg_interp,xerr=0,yerr=df.SAA_eps_conf_interp,marker='x',c='g',ls='-')
+#plt.errorbar(df.N,df.SAA_eps_avg_interp,xerr=0,yerr=df.SAA_eps_conf_interp,marker='x',c='g',ls='-')
 
 plt.axis([4, 16, 0.05, 0.25])
 plt.xlabel('N')
